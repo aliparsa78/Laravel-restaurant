e5ab296a48012a38e9655485c675744f63d33858
aliparsa <aliparsa9@gmail.com>
2023-09-01 13:54:40 +0430

    Adding home template for website

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',[HomeController::class,'index']);
+
 
 Route::middleware([
     'auth:sanctum',
